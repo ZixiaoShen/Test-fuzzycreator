cf458e75473a7bfba084b09ee83ae990115d8e6d
Zixiao Shen <ZealShen>
2019-10-08 22:10:55 +0100

    changes

--- Examples/discrete_t1_fuzzy_set.py
+++ Examples/discrete_t1_fuzzy_set.py
@@ -6,7 +6,6 @@ from fuzzycreator import visualisations
 
 A_points = {1: Decimal('0.25'), 2: 1, 3: Decimal('0.25')}
 B_points = {2: Decimal('0.33'), 3: Decimal('0.66'), 4: Decimal('0.33')}
-
 A = DiscreteT1FuzzySet(A_points)
 B = DiscreteT1FuzzySet(B_points)
 
@@ -15,3 +14,5 @@ visualisations.plot_sets((A, B))
 print(A.calculate_membership(1))
 print(A.calculate_alpha_cut(Decimal('0.25')))
 print(A.calculate_centroid())
+
+print('s(A, B) =', similarity_t1.jaccard(A, B))
