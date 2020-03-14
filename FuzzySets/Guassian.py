cf9b7a0fced1aef2f9c8e2fbca24a63c84ed5eb4
ZixiaoShen <zealshen@outlook.com>
2020-03-14 13:21:27 +0000

    changes

--- FuzzySets/Guassian.py
+++ FuzzySets/Guassian.py
@@ -1,4 +1,3 @@
-from fuzzycreator.membership_functions.triangular import Triangular
 from fuzzycreator.membership_functions.gaussian import Gaussian
 from fuzzycreator.fuzzy_sets.fuzzy_set import FuzzySet
 from fuzzycreator import visualisations
@@ -9,20 +8,23 @@ from fuzzycreator.measures import compatibility_t1
 
 
 gs.global_uod = (0, 1)
-A = FuzzySet(Triangular(0, 0.23, 0.4))
-B = FuzzySet(Gaussian(0.34, 0.1))
-C = FuzzySet(Gaussian(0.53, 0.14))
+A = FuzzySet(Gaussian(0.5, 0.05))
+B = FuzzySet(Gaussian(0.5, 0.12))
+C = FuzzySet(Gaussian(0.5, 0.23))
 
 visualisations.plot_sets((A, B, C))
 
+print('Entropy:')
 print(entropy_t1.kosko(A))
 print(entropy_t1.kosko(B))
 print(entropy_t1.kosko(C))
 
+print('Inclusion:')
 print(inclusion_t1.sanchez(A, B))
 print(inclusion_t1.sanchez(A, C))
 print(inclusion_t1.sanchez(B, C))
 
+print('Compatibility:')
 print(compatibility_t1.compatibility(A, B))
 print(compatibility_t1.compatibility(B, C))
 print(compatibility_t1.compatibility(A, C))
